7b0a61f256643956b0a010d3fc05bc0e66c649a9
vandanghuy9 <106438610+vandanghuy9@users.noreply.github.com>
2022-09-06 08:32:46 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
 var menuItemsUrl =
   "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
-var menuItemsTitleHtml = "snippets/menu-items-title.html";
+var menuItemsTitleHtml = "snippets/menu-item-title.html";
 var menuItemHtml = "snippets/menu-item.html";
 
 // Convenience function for inserting innerHTML for 'select'
