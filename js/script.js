fcafecf09b3c7ce236d89999adfb9cea76ecf7ff
vandanghuy9 <106438610+vandanghuy9@users.noreply.github.com>
2022-09-06 08:33:22 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -43,21 +43,6 @@ var insertProperty = function (string, propName, propValue) {
   return string;
 }
 
-// Remove the class 'active' from home and switch to Menu button
-var switchMenuToActive = function () {
-  // Remove 'active' from home button
-  var classes = document.querySelector("#navHomeButton").className;
-  classes = classes.replace(new RegExp("active", "g"), "");
-  document.querySelector("#navHomeButton").className = classes;
-
-  // Add 'active' to menu button if not already there
-  classes = document.querySelector("#navMenuButton").className;
-  if (classes.indexOf("active") == -1) {
-    classes += " active";
-    document.querySelector("#navMenuButton").className = classes;
-  }
-};
-
 // On page load (before images or CSS)
 document.addEventListener("DOMContentLoaded", function (event) {
 
